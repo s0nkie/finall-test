f1170fae2b10c9d2ba7a489d8cba1ed51c1e4678
s0nkie <2h2kme@gmail.com>
2022-01-22 23:00:50 +0200

    transition between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,10 +8,6 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
-        LoginPage.should_be_login_page(self)
-        # login_form = LoginPage.should_be_login_form(self)
-        # register_form = LoginPage.should_be_register_form(self)
-
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "#Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -4,19 +4,20 @@ from .pages.login_page import LoginPage
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-# def test_guest_can_go_to_login_page(browser):
-#     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-#     page = MainPage(browser, link)
-#     # открываем страницу
-#     page.open()
-#     # выполняем метод страницы — переходим на страницу логина
-#     page.go_to_login_page()
+def test_guest_can_go_to_login_page(browser):
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    # открываем страницу
+    page.open()
+    # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
 
-
-# def test_guest_should_see_login_link(browser):
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
 def test_should_be_login_page(browser):
